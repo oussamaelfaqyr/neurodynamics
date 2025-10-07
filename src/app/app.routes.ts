9b9500dbe865f8c343ba7f9479b9b0e2c644b76a
Oussama Elfaqyr <139490596+oussamaelfaqyr@users.noreply.github.com>
2025-10-07 13:47:04 +0100

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,17 +4,20 @@ import { About } from './pages/about/about';
 import { MarcEvent } from './pages/marc-event/marc-event';
 import { provideRouter } from '@angular/router';
 import { ApplicationConfig } from '@angular/core';
-
+import { JoinusComponent } from '.components/joinus.component';
 
 export const routes: Routes = [
   { path: 'home', component: Home },
   { path: 'about', component: About},
   { path: 'marc', component: MarcEvent},
+  { path: 'joinus', component: JoinusComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' }, // Default redirect
   { path: '**', redirectTo: 'home' }, // Wildcard route for 404
+  
 
 
 
 ]
 
 ;
+
